c4e00dec9fab7b99c8def905922d1b42f8418982
大森優太 <>
2020-08-05 17:31:27 +0900

    add service

--- src/pages/services/index.en.js
+++ src/pages/services/index.en.js
@@ -5,6 +5,8 @@ import SEO from "../../components/seo"
 import Card from "../../components/card"
 import H2Section from "../../components/h2_section"
 import "./service.css"
+import TrashManDatabaseImg from "../../images/services/trash-man-db.jpg"
+import QuizPartyImg from "../../images/services/quiz-party.jpg"
 
 const Services = props => (
   <Layout location={props.location}>
@@ -15,8 +17,14 @@ const Services = props => (
           name="Kuzuo Database"
           description="It is a posting site that writes a man who thinks it is a waste in
           everyday life to the extent that it does not slander himself."
-          imgSrc="https://www.pakutaso.com/shared/img/thumb/MIZUHO19217003_TP_V.jpg"
+          imgSrc={TrashManDatabaseImg}
           link="https://trash-man-db.forest-services.net/"
+        />
+        <Card
+          name="Quiz Party"
+          description="This is a site where you can set quizzes and answer easily."
+          imgSrc={QuizPartyImg}
+          link="https://quiz-party.forest-services.net/"
         />
       </div>
     </H2Section>
--- src/pages/services/index.js
+++ src/pages/services/index.js
@@ -5,6 +5,8 @@ import SEO from "../../components/seo"
 import Card from "../../components/card"
 import H2Section from "../../components/h2_section"
 import "./service.css"
+import TrashManDatabaseImg from "../../images/services/trash-man-db.jpg"
+import QuizPartyImg from "../../images/services/quiz-party.jpg"
 
 
 const Services = props => (
@@ -15,8 +17,14 @@ const Services = props => (
         <Card
           name="クズ男データベース"
           description="日常生活の中でクズだと思った男性について個人の中傷にならない範囲で情報を共有する投稿サイトです。"
-          imgSrc="https://www.pakutaso.com/shared/img/thumb/MIZUHO19217003_TP_V.jpg"
+          imgSrc={TrashManDatabaseImg}
           link="https://trash-man-db.forest-services.net/"
+        />
+        <Card
+          name="クイズパーティー"
+          description="クイズを出題したり、簡単に他の人のクイズに回答できるクイズプラットフォームです。"
+          imgSrc={QuizPartyImg}
+          link="https://quiz-party.forest-services.net/"
         />
       </div>
     </H2Section>
